c4e7d8da8bb8f017e6c28dc0a22b2a7d6558b7e4
UnixH <jonathan_costa_new@live.com>
2023-06-08 14:49:00 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,91 +1,128 @@
-// Selecionando elementos importantes
-const balance = document.getElementById('balance');
-const money_plus = document.getElementById('money-plus');
-const money_minus = document.getElementById('money-minus');
-const form = document.getElementById('form');
-const text = document.getElementById('text');
-const amount = document.getElementById('amount');
-const transactions = document.getElementById('transactions');
+// Getting references to HTML elements
+let balance = document.getElementById('balance');
+let money_plus = document.getElementById('money-plus');
+let money_minus = document.getElementById('money-minus');
+let list = document.getElementById('transactions');
+let form = document.getElementById('form');
+let textInput = document.getElementById('text');
+let amountInput = document.getElementById('amount');
+let categoryInput = document.getElementById('category');
 
-// Array de transações
-let transactionsArray = JSON.parse(localStorage.getItem('transactions')) || [];
+let filterType = document.getElementById('filter-type');
+let filterCategory = document.getElementById('filter-category');
 
-// Função para adicionar transação
-function addTransaction(e) {
-  e.preventDefault();
+// Getting stored transactions from local storage
+let transactions = JSON.parse(localStorage.getItem('transactions')) != null ? 
+                  JSON.parse(localStorage.getItem('transactions')) : [];
 
-  if (text.value.trim() === '' || amount.value.trim() === '' || isNaN(amount.value) || Number(amount.value) === 0) {
-    alert('Por favor, preencha corretamente os campos de nome e valor da transação.');
-  } else {
-    const transaction = {
-      id: generateID(),
-      text: text.value,
-      amount: +amount.value
-    };
-
-    transactionsArray.push(transaction);
-
-    addTransactionToDOM(transaction);
-    updateValues();
-
-    text.value = '';
-    amount.value = '';
-
-    updateLocalStorage();
-  }
-}
-
-// Função para gerar um ID
+// Function to generate a random ID
 function generateID() {
   return Math.floor(Math.random() * 100000000);
 }
 
-// Função para adicionar transações ao DOM
-function addTransactionToDOM(transaction) {
-  const li = document.createElement('li');
-
-  li.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
-  li.setAttribute('data-id', transaction.id);
-  li.innerHTML = `
-    ${transaction.text} <span>${transaction.amount < 0 ? '-' : '+'}${new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(Math.abs(transaction.amount))}</span>
-    <button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>
-  `;
-
-  transactions.appendChild(li);
+// Function to add a new transaction
+function addTransaction(e) {
+  e.preventDefault();
+  if (textInput.value.trim() === '' || amountInput.value.trim() === '') {
+    alert('Por favor, preencha o nome e o valor da transação.');
+  } else {
+    const transaction = {
+      id: generateID(),
+      text: textInput.value,
+      amount: +amountInput.value,
+      category: categoryInput.value
+    };
+    transactions.push(transaction);
+    addTransactionDOM(transaction);
+    updateValues();
+    updateLocalStorage();
+    textInput.value = '';
+    amountInput.value = '';
+    categoryInput.value = '';
+  }
 }
 
-// Função para atualizar o saldo, a receita e a despesa
-function updateValues() {
-  const amounts = transactionsArray.map(transaction => transaction.amount);
-  const total = amounts.reduce((acc, item) => (acc += item), 0);
-  const income = amounts.filter(item => item > 0).reduce((acc, item) => (acc += item), 0);
-  const expense = (amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) * -1);
-
-  // Utilizando o Intl.NumberFormat para formatar os valores para Reais
-  balance.innerText = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(total);
-  money_plus.innerText = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(income);
-  money_minus.innerText = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(expense);
-}
-
-// Função para remover transação
+// Function to remove a transaction
 function removeTransaction(id) {
-  transactionsArray = transactionsArray.filter(transaction => transaction.id !== id);
-
-  // Removendo a transação do DOM
-  const transactionElement = document.querySelector(`li[data-id="${id}"]`);  // Use o atributo data-id para encontrar a transação correta
-  transactionElement.remove();
-
-  updateValues();
+  transactions = transactions.filter(transaction => transaction.id !== id);
   updateLocalStorage();
+  init();
 }
 
-// Função para atualizar o Local Storage
-function updateLocalStorage() {
-  localStorage.setItem('transactions', JSON.stringify(transactionsArray));
+// Function to add transactions to the DOM
+function addTransactionDOM(transaction) {
+  // Classify if income or expense
+  const type = transaction.amount > 0 ? '+' : '-';
+  const item = document.createElement('li');
+  // Add class based on type
+  item.classList.add(transaction.amount > 0 ? 'plus' : 'minus');
+  item.innerHTML = `
+    ${transaction.text} <span>${type}${formatToCurrency(Math.abs(transaction.amount))}</span> <span>${transaction.category}</span> <button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>
+  `;
+  list.appendChild(item);
 }
+
+// Update balance, income and expense
+function updateValues() {
+  const amounts = transactions.map(transaction => transaction.amount);
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+  const income = amounts.filter(item => item > 0).reduce((acc, item) => (acc += item), 0).toFixed(2);
+  const expense = (amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) * -1).toFixed(2);
+  balance.innerText = `R$${formatToCurrency(total)}`;
+  money_plus.innerText = `R$${formatToCurrency(income)}`;
+  money_minus.innerText = `R$${formatToCurrency(expense)}`;
+}
+
+// Function to update local storage transactions
+function updateLocalStorage() {
+  localStorage.setItem('transactions', JSON.stringify(transactions));
+}
+
+// Function to format numbers as money
+function formatToCurrency(amount) {
+  return amount.toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  });
+}
+
+// Event listeners for filter changes
+filterType.addEventListener('change', filterTransactions);
+filterCategory.addEventListener('change', filterTransactions);
+
+// Function to filter transactions based on user selection
+function filterTransactions() {
+  let type = filterType.value; // 'all', 'income', 'expense'
+  let category = filterCategory.value;
+
+  let filteredTransactions = transactions;
+
+  // Filter based on type
+  if (type !== 'all') {
+    filteredTransactions = filteredTransactions.filter(transaction => 
+      type === 'income' ? transaction.amount > 0 : transaction.amount < 0
+    );
+  }
+
+  // Filter based on category
+  if (category !== 'all') {
+    filteredTransactions = filteredTransactions.filter(transaction => transaction.category === category);
+  }
+
+  // Clear current transactions from DOM
+  list.innerHTML = '';
+
+  // Add filtered transactions to DOM
+  filteredTransactions.forEach(addTransactionDOM);
+}
+
+// Init app
+function init() {
+  list.innerHTML = '';
+  transactions.forEach(addTransactionDOM);
+  updateValues();
+}
+
+init();
 
 form.addEventListener('submit', addTransaction);
-
-// Inicializando a aplicação
-transactionsArray.forEach(addTransactionToDOM);
-updateValues();
